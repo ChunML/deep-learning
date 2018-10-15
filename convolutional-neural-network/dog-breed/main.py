9ab1b1016704ef9cb366d54a5ab791012110dc28
Vinhtrung Tran (Ｔｒａｎ ＶＴ GE) <RJ032072@jpn.mds.honda.com>
2018-10-15 18:17:12 +0900

    Reimplemented network using tf.contrib.layers. Configured learning rate to decay over time. Added summary for tracking purpose.

--- convolutional-neural-network/dog-breed/main.py
+++ convolutional-neural-network/dog-breed/main.py
@@ -11,9 +11,12 @@ def main():
     filenames, labels, int_to_breed, breed_to_int = read_labels_from_file(
         label_path)
     features, labels = input_fn(filenames, labels, batch_size)
-    net = Network(features, labels, new_size, 3, len(int_to_breed))
     num_batches = len(filenames) // batch_size
     num_epochs = 100
+    num_epochs_per_decay = 2
+    decay_steps = num_epochs_per_decay * num_batches
+    net = Network(features, labels, len(int_to_breed),
+                  initial_learning_rate=0.01, decay_steps=decay_steps)
     train_info = net.train(num_epochs, num_batches)
     train_df = pd.DataFrame(train_info)
     train_df.to_csv('train_info_{}_epochs.csv'.format(num_epochs))
--- convolutional-neural-network/dog-breed/network.py
+++ convolutional-neural-network/dog-breed/network.py
@@ -7,17 +7,17 @@ from collections import defaultdict
 
 
 class Network(object):
-    def __init__(self, images, labels, image_size, channel, ouput_size):
+    def __init__(self, images, labels, ouput_size, initial_learning_rate, decay_steps, decay_factor=0.94):
         with tf.name_scope('conv1') as scope:
-            pool1 = self._build_conv2d_block(images, scope, 3, 64, 2)
+            pool1 = self._build_conv2d_block(images, scope, 64, 2)
         with tf.name_scope('conv2') as scope:
-            pool2 = self._build_conv2d_block(pool1, scope, 64, 128, 2)
+            pool2 = self._build_conv2d_block(pool1, scope, 128, 2)
         with tf.name_scope('conv3') as scope:
-            pool3 = self._build_conv2d_block(pool2, scope, 128, 128, 3)
+            pool3 = self._build_conv2d_block(pool2, scope, 256, 3)
         with tf.name_scope('conv4') as scope:
-            pool4 = self._build_conv2d_block(pool3, scope, 128, 256, 3)
+            pool4 = self._build_conv2d_block(pool3, scope, 512, 3)
         with tf.name_scope('conv5') as scope:
-            pool5 = self._build_conv2d_block(pool4, scope, 256, 512, 3)
+            pool5 = self._build_conv2d_block(pool4, scope, 512, 3)
 
         pool5_shape = pool5.get_shape().as_list()
         flatten = tf.reshape(
@@ -32,21 +32,63 @@ class Network(object):
         self.predictions = tf.argmax(logits, axis=1)
         self.accuracy = tf.reduce_mean(
             tf.cast(tf.equal(self.predictions, labels), tf.float32))
+        tf.summary.scalar('accuracy', self.accuracy)
 
         self.cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(
             logits=logits, labels=labels)
         self.loss = tf.reduce_mean(self.cross_entropy)
-        self.opt = tf.train.GradientDescentOptimizer(0.001).minimize(self.loss)
+        tf.summary.scalar('loss', self.loss)
 
-    def train(self, num_epochs, num_batches):
+        self.global_step = tf.train.get_or_create_global_step()
+        self.learning_rate = tf.train.exponential_decay(initial_learning_rate,
+                                                        self.global_step,
+                                                        decay_steps,
+                                                        decay_factor,
+                                                        staircase=True)
+        self.opt = tf.train.GradientDescentOptimizer(
+            self.learning_rate).minimize(self.loss)
+        self.merged = tf.summary.merge_all()
+
+    def _conv2d(self, inputs, name, out_channels):
+        conv_relu = tf.contrib.layers.conv2d(
+            inputs, out_channels, [3, 3], scope=name)
+        tf.summary.histogram(name, conv_relu)
+        return conv_relu
+
+    def _max_pool(self, conv_relu, name):
+        pool = tf.contrib.layers.max_pool2d(conv_relu, [2, 2], scope=name)
+        tf.summary.histogram(name, pool)
+        return pool
+
+    def _build_conv2d_block(self, inputs, name, out_channels, num_layers):
+        conv_relu = inputs
+        for i in range(num_layers):
+            conv_relu = self._conv2d(conv_relu, '{}_{}'.format(
+                name, i), out_channels)
+
+        pool = self._max_pool(conv_relu, '{}_pool'.format(name))
+        return pool
+
+    def _fc(self, inputs, name, ouput_size, activate_func=tf.nn.relu):
+        fc = tf.contrib.layers.fully_connected(
+            inputs, ouput_size, activation_fn=activate_func)
+        tf.summary.histogram(name, fc)
+        return fc
+
+    def train(self, num_epochs, num_batches, checkpoint_fn='checkpoints/dogs.ckpt'):
         train_info = defaultdict(list)
+        saver = tf.train.Saver()
         with tf.Session() as sess:
+            train_writer = tf.summary.FileWriter('logdir', sess.graph)
             sess.run(tf.global_variables_initializer())
             for epoch_i in range(num_epochs):
                 for batch_i in range(num_batches):
                     start_time = time()
                     _, loss, acc = sess.run(
                         [self.opt, self.loss, self.accuracy])
+                    if batch_i % 50 == 0:
+                        summary = sess.run(self.merged)
+                        train_writer.add_summary(summary, self.global_step)
                     train_info['loss'].append(loss)
                     train_info['acc'].append(acc)
                     batch_time = time() - start_time
@@ -56,59 +98,5 @@ class Network(object):
                     hours, minutes, seconds = converse_time(time_left)
                     print('Epoch: {} Batch: {}/{} Loss: {:.4f} Accuracy: {:.4f} Time left: {:.0f}:{:.0f}:{:.0f}'.format(
                         epoch_i + 1, batch_i, num_batches, loss, acc, hours, minutes, seconds))
-
+            saver.save(sess, checkpoint_fn)
         return train_info
-
-    def _conv2d(self, inputs, name, in_channels, out_channels):
-        with tf.variable_scope(name) as scope:
-            weights = tf.get_variable(
-                'weights',
-                [3, 3, in_channels, out_channels],
-                initializer=tf.truncated_normal_initializer(stddev=5e-2),
-                dtype=tf.float32)
-            biases = tf.get_variable(
-                'biases',
-                [out_channels],
-                initializer=tf.constant_initializer(0.0),
-                dtype=tf.float32)
-            conv = tf.nn.conv2d(inputs, weights,
-                                strides=[1, 1, 1, 1],
-                                padding='SAME')
-            conv_relu = tf.nn.relu(tf.nn.bias_add(
-                conv, biases), name=scope.name)
-        return conv_relu
-
-    def _max_pool(self, conv_relu, name):
-        with tf.variable_scope(name) as scope:
-            return tf.nn.max_pool(conv_relu, ksize=[1, 2, 2, 1],
-                                  strides=[1, 2, 2, 1],
-                                  padding='SAME',
-                                  name=scope.name)
-
-    def _build_conv2d_block(self, inputs, name, in_channels, out_channels, num_layers):
-        for i in range(num_layers):
-            conv_relu = self._conv2d(inputs, '{}_{}'.format(
-                name, i), in_channels, out_channels)
-            inputs = conv_relu
-            in_channels = out_channels
-
-        pool = self._max_pool(conv_relu, '{}_pool'.format(name))
-        return pool
-
-    def _fc(self, inputs, name, ouput_size, activate_func=tf.nn.relu):
-        with tf.variable_scope(name) as scope:
-            weights = tf.get_variable(
-                'weights', [inputs.get_shape()[-1], ouput_size],
-                initializer=tf.truncated_normal_initializer(stddev=0.04),
-                dtype=tf.float32)
-            biases = tf.get_variable(
-                'biases', [ouput_size],
-                initializer=tf.constant_initializer(0.0),
-                dtype=tf.float32)
-            if activate_func:
-                fc = activate_func(
-                    tf.matmul(inputs, weights) + biases, name=scope.name)
-            else:
-                fc = tf.identity(tf.matmul(inputs, weights) +
-                                 biases, name=scope.name)
-        return fc
